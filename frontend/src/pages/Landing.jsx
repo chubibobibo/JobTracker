3017549ab765f2aa71ccb7a23d14c0fcc5a21e09
Alistair Abao <abaolester@gmail.com>
2023-12-21 10:45:50 +0100

    updated responsiveness

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -20,37 +20,35 @@ function Landing() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Container maxWidth='lg'>
-        <Grid container spacing={2}>
-          <Grid sm={4} className='landingGrid1'>
-            <Item>
-              <h1>Landing Page</h1>
-              <h3 className='landingPar'>
-                <img src='favicon.ico' alt='' className='landingIcon' />
-                Job Tracker App
-              </h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Distinctio totam doloribus error nam tenetur aperiam omnis culpa
-                eos, placeat nihil vel quisquam harum obcaecati voluptas,
-                eveniet pariatur eius enim repellendus.
-              </p>
-              <Link to='/register'>Register</Link>
-              <br />
-              <Link to='/login'>Login</Link>
-            </Item>
-          </Grid>
-          <Grid sm={8} className='landingGrid2'>
-            <Item>
-              <img
-                src='https://images.unsplash.com/photo-1519720842496-c64a0d0627f9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fGpvYiUyMGFwcGxpY2F0aW9ufGVufDB8fDB8fHww'
-                alt=''
-                className='landingLogo'
-              />
-            </Item>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid sm={10} md={6} className='landingGrid1'>
+          <Item>
+            <h1 className='landingPar'>
+              <img src='favicon.ico' alt='' className='landingIcon' />
+              Job Tracker App
+            </h1>
+            <h3>Track and manage your job applications</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Distinctio totam doloribus error nam tenetur aperiam omnis culpa
+              eos, placeat nihil vel quisquam harum obcaecati voluptas, eveniet
+              pariatur eius enim repellendus.
+            </p>
+            <Link to='/register'>Register</Link>
+            <br />
+            <Link to='/login'>Login</Link>
+          </Item>
         </Grid>
-      </Container>
+        <Grid md={6} sm={0} xs={0} className='landingGrid2'>
+          <div>
+            <img
+              src='https://images.unsplash.com/photo-1519720842496-c64a0d0627f9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fGpvYiUyMGFwcGxpY2F0aW9ufGVufDB8fDB8fHww'
+              alt=''
+              className='landingLogo'
+            />
+          </div>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
