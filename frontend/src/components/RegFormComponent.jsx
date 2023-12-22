081a9341b8eb6608f50e6c84a49a71964cdd5b3e
Alistair Abao <abaolester@gmail.com>
2023-12-22 14:38:21 +0100

    refactored reg form

---
+++ frontend/src/components/RegFormComponent.jsx
@@ -0,0 +1,24 @@
+//import material UI
+import TextField from "@mui/material/TextField";
+
+//custom css
+import "../utils/styles/registerFormContainer.css";
+
+// eslint-disable-next-line react/prop-types
+function RegFormComponent({ label, name, type }) {
+  return (
+    <div>
+      <div className='registerFormContainer'>
+        {/* name */}
+        <TextField
+          id={name}
+          label={label}
+          variant='outlined'
+          size='small'
+          type={type}
+        />
+      </div>
+    </div>
+  );
+}
+export default RegFormComponent;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,69 +1,32 @@
-import { Link } from "react-router-dom";
-//import material UI
-import { FormControl } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+//import formComponent
+import RegFormComponent from "../components/RegFormComponent.jsx";
 
 //custom css
 import "../utils/styles/registerFormContainer.css";
+
+//matertialUI
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import Stack from "@mui/material/Stack";
 
 function Register() {
   return (
     <div className='registerContainer'>
       <form action='' className='regForm'>
-        <div className='registerFormContainer'>
-          <Stack spacing={2}>
-            <h1 className='regLabel'>Register</h1>
-            {/* name */}
-            <TextField
-              id='outlined-basic'
-              label='Name'
-              variant='outlined'
-              size='small'
-              type='text'
-            />
-            {/* email */}
-            <TextField
-              id='outlined-basic'
-              label='Email'
-              variant='outlined'
-              size='small'
-              type='email'
-            />
-            {/* password */}
-            <TextField
-              id='outlined-basic'
-              label='Password'
-              variant='outlined'
-              size='small'
-              type='password'
-            />
-            {/* last name */}
-            <TextField
-              id='outlined-basic'
-              label='Last name'
-              variant='outlined'
-              size='small'
-              type='text'
-            />
-            {/* location */}
-            <TextField
-              id='outlined-basic'
-              label='Location'
-              variant='outlined'
-              size='small'
-              type='text'
-            />
-            <p>
-              Already have an account? <Link to='/login'>Login</Link>
-            </p>
-            <Button variant='contained' color='success' type='submit'>
-              Register
-            </Button>
-          </Stack>
-        </div>
+        <Stack spacing={2}>
+          <h1 className='regLabel'>Register</h1>
+          <RegFormComponent type='text' label='name' name='name' />
+          <RegFormComponent type='email' label='email' name='email' />
+          <RegFormComponent type='password' label='password' name='password' />
+          <RegFormComponent type='text' label='last name' name='lastName' />
+          <RegFormComponent type='text' label='location' name='location' />
+          <p>
+            Already have an account? <Link to='/login'>Login</Link>
+          </p>
+          <Button variant='contained' color='success' type='submit'>
+            Register
+          </Button>
+        </Stack>
       </form>
     </div>
   );
