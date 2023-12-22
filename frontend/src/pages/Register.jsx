2bea72cf3673af11c4711dae6acc95fee5cf218d
Alistair Abao <abaolester@gmail.com>
2023-12-22 14:08:27 +0100

    createdd skeleton register form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,70 @@
 import { Link } from "react-router-dom";
+//import material UI
+import { FormControl } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+//custom css
+import "../utils/styles/registerFormContainer.css";
+
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-      <p>Already have an account?</p>
-      <Link to='/login'>Login</Link>
+    <div className='registerContainer'>
+      <form action='' className='regForm'>
+        <div className='registerFormContainer'>
+          <Stack spacing={2}>
+            <h1 className='regLabel'>Register</h1>
+            {/* name */}
+            <TextField
+              id='outlined-basic'
+              label='Name'
+              variant='outlined'
+              size='small'
+              type='text'
+            />
+            {/* email */}
+            <TextField
+              id='outlined-basic'
+              label='Email'
+              variant='outlined'
+              size='small'
+              type='email'
+            />
+            {/* password */}
+            <TextField
+              id='outlined-basic'
+              label='Password'
+              variant='outlined'
+              size='small'
+              type='password'
+            />
+            {/* last name */}
+            <TextField
+              id='outlined-basic'
+              label='Last name'
+              variant='outlined'
+              size='small'
+              type='text'
+            />
+            {/* location */}
+            <TextField
+              id='outlined-basic'
+              label='Location'
+              variant='outlined'
+              size='small'
+              type='text'
+            />
+            <p>
+              Already have an account? <Link to='/login'>Login</Link>
+            </p>
+            <Button variant='contained' color='success' type='submit'>
+              Register
+            </Button>
+          </Stack>
+        </div>
+      </form>
     </div>
   );
 }
